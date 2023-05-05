54b7174f39694877e3502dbb26405c351ec224da
Matteo <mcombi@redhat.com>
2023-05-05 14:35:15 +0200

    kakfa configuration

--- src/main/java/com/example/combi/demo/service/OrdersConsumer.java
+++ src/main/java/com/example/combi/demo/service/OrdersConsumer.java
@@ -4,10 +4,12 @@ package com.example.combi.demo.service;
 import com.example.combi.demo.model.Order;
 import com.example.combi.demo.model.OrderEntity;
 import com.example.combi.demo.repository.OrderRepository;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,17 +22,18 @@ public class OrdersConsumer {
     private final Logger logger = LoggerFactory.getLogger(OrdersConsumer.class);
     @Autowired
     OrderRepository orderRepository;
-    @KafkaListener(topics = "orders",containerFactory="orderKafkaListenerContainerFactory")
-    public void consume(String uuid, Order message) throws IOException {
-        logger.info(String.format("#### -&gt; Consumed message -&gt; %s", message));
+    @KafkaListener(topics = "orders")
+    public void consume(ConsumerRecord<String, Order> cr,
+                        @Payload Order payload) throws IOException {
+        logger.info(String.format("#### -&gt; Consumed message -&gt; %s", payload));
 
 
-        Optional<OrderEntity> oe = orderRepository.findById(message.id);
+        Optional<OrderEntity> oe = orderRepository.findById(payload.id);
         if (oe.isEmpty()) {
             OrderEntity record=oe.get();
-            record.description= message.description;
+            record.description= payload.description;
             record.quantity = record.quantity;
-            record.id= Math.toIntExact((Long) message.id);
+            record.id= Math.toIntExact((Long) payload.id);
             orderRepository.save(record);
             logger.info("message persisted successfully");
         }
